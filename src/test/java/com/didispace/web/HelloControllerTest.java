b6accb87284bf797cacfd16826693df0f409c5c8
李蕊 <lirui@Campusnetwork.net.hziee.edu.cn>
2017-10-19 22:36:37 +0800

    Junit test

---
+++ src/test/java/com/didispace/web/HelloControllerTest.java
@@ -0,0 +1,15 @@
+package com.didispace.web;
+
+import org.junit.Assert;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class HelloControllerTest {
+    @org.junit.jupiter.api.Test
+    void index() {
+
+    }
+
+}

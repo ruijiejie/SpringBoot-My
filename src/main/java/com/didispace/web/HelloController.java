44e98a8097592c95b7001da853e411be9c279558
李蕊 <lirui@Campusnetwork.net.hziee.edu.cn>
2017-10-20 19:41:50 +0800

    add test demo

--- src/main/java/com/didispace/web/HelloController.java
+++ src/main/java/com/didispace/web/HelloController.java
@@ -3,6 +3,8 @@ package com.didispace.web;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/*
+* */
 @RestController
 public class HelloController {
     @RequestMapping("/hello")
